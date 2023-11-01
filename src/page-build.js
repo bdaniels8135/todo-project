a1b69c71bb1a2233e2cf35952c41cd004b7320f0
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-10-31 23:59:10 -0400

    Add icons to sidebar and add a header for app name

--- src/page-build.js
+++ src/page-build.js
@@ -1,3 +1,9 @@
+import allIcon from './img/inbox.svg'
+import todayIcon from './img/calendar-today.svg'
+import upcomingIcon from './img/calendar-search.svg'
+import importantIcon from './img/star.svg'
+import plusIcon from './img/plus-thick.svg'
+
 const body = document.querySelector('body')
 
 // Add Header
@@ -13,21 +19,47 @@ header.appendChild(headerText);
 const sidebar = document.createElement('aside');
 body.appendChild(sidebar);
 
-// Include filters nav
-const filterNav = document.createElement('ul');
-filterNav.id = 'filter-nav';
-sidebar.appendChild(filterNav);
+// Include tasks nav
+const taskNav = document.createElement('ul');
+taskNav.id = 'task-nav';
+sidebar.appendChild(taskNav);
 
-const filterNavHeader = document.createElement('h1');
-filterNavHeader.innerText = 'Tasks by Date';
-filterNav.appendChild(filterNavHeader);
+const taskNavHeader = document.createElement('h1');
+taskNavHeader.innerText = 'Tasks';
+taskNav.appendChild(taskNavHeader);
 
-const filterLabels = ['Today', 'This Week', 'This Month', 'Important'];
-for (let text of filterLabels) {
+const taskLabelIconPairs = [
+    {
+        label: 'All',
+        icon: allIcon,
+    },
+    {
+        label: 'Today',
+        icon: todayIcon,
+    },
+    {
+        label: 'Upcoming',
+        icon: upcomingIcon,
+    },
+    {
+        label: 'Important',
+        icon: importantIcon,
+    },
+];
+
+for (let pair of taskLabelIconPairs) {
     const newItem = document.createElement('li');
-    newItem.innerText = text;
-    newItem.id = `${text.toLowerCase().replace(' ', '-')}-btn`;
-    filterNav.appendChild(newItem);
+    newItem.id = `${pair.label.toLowerCase().replace(' ', '-')}-btn`;
+    taskNav.appendChild(newItem);
+
+    const newItemIcon = document.createElement('img');
+    newItemIcon.src = pair.icon;
+    newItem.appendChild(newItemIcon);
+    
+    const newItemText = document.createElement('p');
+    newItemText.innerText = pair.label;
+    newItem.appendChild(newItemText);
+    
 }
 
 // Include projects nav
@@ -36,8 +68,22 @@ projectsNav.id = 'projects-nav';
 sidebar.appendChild(projectsNav);
 
 const projectsHeader = document.createElement('h1');
-projectsHeader.innerText = 'Tasks by Project';
+projectsHeader.innerText = 'Projects';
 projectsNav.appendChild(projectsHeader);
+
+// Add new project button
+const newProjectBtn = document.createElement('li');
+newProjectBtn.id = 'new-project-btn';
+projectsNav.appendChild(newProjectBtn);
+
+const newProjectBtnIcon = document.createElement('img');
+newProjectBtnIcon.src = plusIcon;
+newProjectBtn.appendChild(newProjectBtnIcon);
+
+const newProjectBtnText = document.createElement('p');
+newProjectBtnText.innerText = 'New Project';
+newProjectBtn.appendChild(newProjectBtnText);
+
 
 // Include Main element
 const main = document.createElement('main');
