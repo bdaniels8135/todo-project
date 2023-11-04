3d462fcbbe158aa6579cea6a36ab11447d8bbead
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-04 18:31:35 -0400

    Create test tasks and implement display function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import './style.css';
 import './page-build.js';
 import { Task } from './task.js';
 import { Tag } from './tag.js';
+import { displayTasks } from './display-controller.js'
+import { isSameDay, isPast, endOfDay } from 'date-fns';
+
+const tasks = [];
+
+const tags = [];
+
+const testTask1 = new Task('Test Task 1', new Date(2025, 11, 5), 'This is the first test task.');
+tasks.push(testTask1);
+
+const testTask2 = new Task('Test Task 2', endOfDay(new Date()), 'This is the second test task.');
+tasks.push(testTask2);
+
+const testTask3 = new Task('Test Task 3', new Date(2020, 5, 10), 'This is the third test task.');
+tasks.push(testTask3);
+
+const importantTag = new Tag('Important', 'rgb(255, 0, 0)');
+tags.push(importantTag);
+testTask2.addTag(importantTag);
+
+const testTag2 = new Tag('Test Tag 2', 'rgb(100, 100, 100)');
+tags.push(testTag2);
+testTask1.addTag(testTag2);
 
 (() => {
     const BUTTONS = {
@@ -18,12 +41,11 @@ import { Tag } from './tag.js';
         BUTTONS[key].addEventListener('click', () => {eval(newClickFn)()});
     }
 
-    const resolveAllBtnClick = () => {
-        console.log('All Button Pressed!');
-    }
+    const resolveAllBtnClick = () => { displayTasks(tasks) }
 
     const resolveTodayBtnClick = () => {
-        console.log('Today Button Pressed!');
+        const todayTasks = tasks.filter(task => isSameDay(new Date(), task.dueDate));
+        displayTasks(todayTasks);
     }
 
     const resolveUpcomingBtnClick = () => {
@@ -31,7 +53,8 @@ import { Tag } from './tag.js';
     }
 
     const resolvePastDueBtnClick = () => {
-        console.log('Past Due Button Pressed!')
+        const pastDueTasks = tasks.filter(task => isPast(task.dueDate));
+        displayTasks(pastDueTasks);
     }
 
     const resolveNewTaskBtnClick = () => {
@@ -41,4 +64,6 @@ import { Tag } from './tag.js';
     const resolveNewTagBtnClick = () => {
         console.log('New Tag Button Pressed!');
     }
-})();
+
+    displayTasks(tasks);
+})()
