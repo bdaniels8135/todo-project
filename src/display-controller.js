513b1d73b3487f19ca2f998bd43fa7fcedbf5579
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-04 23:32:16 -0400

    Make task details content

--- src/display-controller.js
+++ src/display-controller.js
@@ -34,8 +34,72 @@ export function displayTasks(tasksToDisplay) {
         newDueDateCell.classList.add('due-date-cell');
         newDueDateCell.innerText = format(task.dueDate, 'MM/dd/yyyy');
         newRow.appendChild(newDueDateCell);
+        newRow.addEventListener('click', () => displayTask(task))
         taskTable.appendChild(newRow);
     }
+}
+
+function displayTask(task) {
+    clearMain();
+    const taskCard = document.createElement('form');
+    main.appendChild(taskCard);
+    
+    const titleInputLabel = document.createElement('label');
+    titleInputLabel.innerText = 'Title:';
+    taskCard.appendChild(titleInputLabel);
+    const titleInput = document.createElement('input');
+    titleInput.id = 'title-input';
+    titleInput.type = 'text';
+    titleInput.value = task.title;
+    taskCard.appendChild(titleInput);
+    
+    const dueDateInputLabel = document.createElement('label');
+    dueDateInputLabel.innerText = 'Due Date:';
+    taskCard.appendChild(dueDateInputLabel);
+    const dueDateInput = document.createElement('input');
+    dueDateInput.id = 'due-date-input';
+    dueDateInput.type = 'date';
+    dueDateInput.value = format(task.dueDate, 'yyyy-MM-dd');
+    taskCard.appendChild(dueDateInput);
+
+    const descInputLabel = document.createElement('label');
+    descInputLabel.innerText = 'Description:';
+    taskCard.appendChild(descInputLabel);
+    const descInput = document.createElement('input');
+    descInput.id = 'desc-input';
+    descInput.type = 'text';
+    descInput.value = task.shortDesc;
+    taskCard.appendChild(descInput);
+
+    const notesInputLabel = document.createElement('label');
+    notesInputLabel.innerText = 'Notes:';
+    taskCard.appendChild(notesInputLabel);
+    const notesInput = document.createElement('input');
+    notesInput.id = 'notes-input';
+    notesInput.type = 'textarea';
+    notesInput.value = task.notes;
+    taskCard.appendChild(notesInput);
+
+    const checklistLabel = document.createElement('label');
+    checklistLabel.innerText = 'Checklist:';
+    taskCard.appendChild(checklistLabel);
+    const checklist = document.createElement('ul');
+    checklist.classList.add('checklist');
+    checklist.innerText = 'Check List goes here!';
+    taskCard.appendChild(checklist);
+
+    const tagsLabel = document.createElement('label');
+    tagsLabel.innerText = 'Tags:';
+    taskCard.appendChild(tagsLabel);
+    const tags = document.createElement('ul');
+    tags.classList.add('tags-list');
+    tags.innerText = 'List of Tags goes here!';
+    taskCard.appendChild(tags);
+
+    const saveBtn = document.createElement('button');
+    saveBtn.type = 'button';
+    saveBtn.innerText = 'Save Changes';
+    taskCard.appendChild(saveBtn);
 }
 
 export function displayDateInput() {
