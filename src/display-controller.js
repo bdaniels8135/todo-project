9965a87e55ec16c1a5198883401948df1e9f1f38
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-05 18:36:07 -0500

    Style the task form display

--- src/display-controller.js
+++ src/display-controller.js
@@ -39,62 +39,73 @@ export function displayTasks(tasksToDisplay) {
     }
 }
 
-function displayTask(task) {
+export function displayTask(task) {
     clearMain();
     const taskCard = document.createElement('form');
     main.appendChild(taskCard);
     
+    const container1 = document.createElement('div');
+    taskCard.appendChild(container1);
     const titleInputLabel = document.createElement('label');
     titleInputLabel.innerText = 'Title:';
-    taskCard.appendChild(titleInputLabel);
+    container1.appendChild(titleInputLabel);
     const titleInput = document.createElement('input');
     titleInput.id = 'title-input';
     titleInput.type = 'text';
     titleInput.value = task.title;
-    taskCard.appendChild(titleInput);
-    
+    titleInput.maxLength = 30;
+    container1.appendChild(titleInput);
     const dueDateInputLabel = document.createElement('label');
     dueDateInputLabel.innerText = 'Due Date:';
-    taskCard.appendChild(dueDateInputLabel);
+    container1.appendChild(dueDateInputLabel);
     const dueDateInput = document.createElement('input');
     dueDateInput.id = 'due-date-input';
     dueDateInput.type = 'date';
     dueDateInput.value = format(task.dueDate, 'yyyy-MM-dd');
-    taskCard.appendChild(dueDateInput);
+    container1.appendChild(dueDateInput);
 
+    const container2 = document.createElement('div');
+    taskCard.appendChild(container2);
     const descInputLabel = document.createElement('label');
     descInputLabel.innerText = 'Description:';
-    taskCard.appendChild(descInputLabel);
+    container2.appendChild(descInputLabel);
     const descInput = document.createElement('input');
     descInput.id = 'desc-input';
     descInput.type = 'text';
     descInput.value = task.shortDesc;
-    taskCard.appendChild(descInput);
+    descInput.maxLength = 45;
+    container2.appendChild(descInput);
 
-    const notesInputLabel = document.createElement('label');
-    notesInputLabel.innerText = 'Notes:';
-    taskCard.appendChild(notesInputLabel);
-    const notesInput = document.createElement('input');
-    notesInput.id = 'notes-input';
-    notesInput.type = 'textarea';
-    notesInput.value = task.notes;
-    taskCard.appendChild(notesInput);
-
-    const checklistLabel = document.createElement('label');
-    checklistLabel.innerText = 'Checklist:';
-    taskCard.appendChild(checklistLabel);
-    const checklist = document.createElement('ul');
-    checklist.classList.add('checklist');
-    checklist.innerText = 'Check List goes here!';
-    taskCard.appendChild(checklist);
-
+    const container5 = document.createElement('div');
+    taskCard.appendChild(container5);
     const tagsLabel = document.createElement('label');
     tagsLabel.innerText = 'Tags:';
-    taskCard.appendChild(tagsLabel);
+    container5.appendChild(tagsLabel);
     const tags = document.createElement('ul');
     tags.classList.add('tags-list');
     tags.innerText = 'List of Tags goes here!';
-    taskCard.appendChild(tags);
+    container5.appendChild(tags);
+
+    const container3 = document.createElement('div');
+    taskCard.appendChild(container3);
+    const notesInputLabel = document.createElement('label');
+    notesInputLabel.innerText = 'Notes:';
+    container3.appendChild(notesInputLabel);
+    const notesInput = document.createElement('textarea');
+    notesInput.setAttribute('oninput', 'this.style.height = ""; this.style.height = this.scrollHeight + 5 + "px"');
+    notesInput.id = 'notes-input';
+    notesInput.value = task.notes;
+    container3.appendChild(notesInput);
+    
+    const container4 = document.createElement('div');
+    taskCard.appendChild(container4);
+    const checklistLabel = document.createElement('label');
+    checklistLabel.innerText = 'Checklist:';
+    container4.appendChild(checklistLabel);
+    const checklist = document.createElement('ul');
+    checklist.classList.add('checklist');
+    checklist.innerText = 'Check List goes here!';
+    container4.appendChild(checklist);
 
     const saveBtn = document.createElement('button');
     saveBtn.type = 'button';
@@ -106,7 +117,7 @@ export function displayDateInput() {
     const dateInputContainer = document.createElement('div');
     const dateInputLabel = document.createElement('label');
     dateInputLabel.innerText = 'Display tasks due between now and:';
-    dateInputLabel.id = 'upcoming-date-label'
+    dateInputLabel.id = 'upcoming-date-label';
     dateInputContainer.appendChild(dateInputLabel);
     const dateInput = document.createElement('input');
     dateInput.type = 'date';
