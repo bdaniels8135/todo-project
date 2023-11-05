b8271fba25bdbb377ac14d2c9b0a7f41cc4effde
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-04 20:13:02 -0400

    Create date input display function

--- src/display-controller.js
+++ src/display-controller.js
@@ -1,12 +1,21 @@
 import { format } from 'date-fns'
 
 const main = document.querySelector('main');
+const mainHeader = document.querySelector('main h1');
+const mainContent = document.querySelector('article');
+
+export function clearContent() {
+    mainContent.innerHTML = '';
+}
+
+export function updateMainHeader(mainHeaderText) {
+    mainHeader.innerText = mainHeaderText;
+}
 
 export function displayTasks(tasksToDisplay) {
-    main.innerHTML = '';
     const taskTable = document.createElement('table');
     taskTable.classList.add('task-table');
-    main.appendChild(taskTable);
+    mainContent.appendChild(taskTable);
 
     for (let task of tasksToDisplay) {
         const newRow = document.createElement('tr');
@@ -21,4 +30,15 @@ export function displayTasks(tasksToDisplay) {
         newRow.appendChild(newDescCell);
         taskTable.appendChild(newRow);
     }
+}
+
+export function displayDateInput() {
+    const dateInputLabel = document.createElement('label');
+    dateInputLabel.innerText = 'Display tasks due between now and:';
+    main.insertBefore(dateInputLabel, mainContent);
+    const dateInput = document.createElement('input');
+    dateInput.type = 'date';
+    dateInput.id = 'upcoming-date';
+    dateInput.min = new Date();
+    main.insertBefore(dateInput, mainContent);
 }
