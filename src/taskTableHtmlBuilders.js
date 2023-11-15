07dcd624d004e6b259a48a050f250ea82869a08c
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-14 22:17:08 -0500

    Fix due date bug

--- src/taskTableHtmlBuilders.js
+++ src/taskTableHtmlBuilders.js
@@ -1,6 +1,8 @@
 import { wrapHtmlElements, buildTableCellHtml } from './htmlBuilders';
+import { format } from 'date-fns';
 
 const NO_TITLE_PLACEHOLDER = '(NO TITLE)';
+const NO_DUE_DATE_PLACEHOLDER = '(NO DATE)';
 
 export function buildTaskTableHtml() {
     const taskTableHtml = document.createElement('table');
@@ -9,10 +11,11 @@ export function buildTaskTableHtml() {
     return taskTableHtml;
 }
 
-export function buildTaskRowHtml(title, shortDescText, dueDateString) {
+export function buildTaskRowHtml(title, shortDescText, dueDate) {
     const titleText = title ? title : NO_TITLE_PLACEHOLDER;
     const titleCellHtml = buildTableCellHtml(titleText, 'title-cell');
     const shortDescCellHtml = buildTableCellHtml(shortDescText, 'short-desc-cell');
+    const dueDateString = !isNaN(dueDate) ? format(dueDate, 'MM/dd/yyyy') : NO_DUE_DATE_PLACEHOLDER;
     const dueDateCellHtml = buildTableCellHtml(dueDateString, 'due-date-cell');
     const taskRowHtml = wrapHtmlElements('tr', titleCellHtml, shortDescCellHtml, dueDateCellHtml);
 
