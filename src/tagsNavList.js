68d8fd3fba886bb406cafbd286ee9ca57dd9e987
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-16 00:13:38 -0500

    Move task nav list logic to a new file

---
+++ src/tagsNavList.js
@@ -0,0 +1,38 @@
+import removeIcon from './img/close-circle.svg';
+import { clearContainer, resolveTagBtnClick } from '.';
+import { wrapHtmlElements, buildIconHtml, buildTextHtml } from './htmlBuilders';
+
+export function buildTagsNavList(tagsNav, tagsList) {
+    return (() => {
+        const HTML = tagsNav.querySelector('ul');
+
+        function _buildTagNavListItemHtml(text) {
+            const tagNavListItemTextHtml = buildTextHtml(text);
+            const tagNavListItemIconHtml = buildIconHtml(removeIcon);
+            const tagNavListItemHtml = wrapHtmlElements('li', tagNavListItemTextHtml, tagNavListItemIconHtml);
+        
+            return tagNavListItemHtml;
+        }
+        
+        function _appendTagItem(tag) {
+            const tagNavListItemHtml = _buildTagNavListItemHtml(tag.text);
+            tagNavListItemHtml.addEventListener('click', () => { resolveTagBtnClick(tag) });
+            HTML.appendChild(tagNavListItemHtml);
+        }
+        
+        function populateTagsNavList() {
+            tagsList.getTags().forEach(tag => { _appendTagItem(tag) });
+        }
+        
+        function updateTagsNavList() {
+            clearContainer(HTML);
+            populateTagsNavList();
+        }
+                
+        return {
+            HTML,
+            updateTagsNavList,
+        }
+
+    })();
+}
